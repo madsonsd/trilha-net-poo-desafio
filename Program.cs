1b20b129d36543ebd6ca916eefe78676c6bf148b
madsonsd <madsonsd@gmail.com>
2024-05-30 11:52:35 -0300

    Desafio POO trilha-net resolvido!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("91929394","C01", "1122334455", 256);
+nokia.Ligar();
+nokia.InstalarAplicativo("DIO");
+
+Console.WriteLine("------------");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone("95969798","15 PRO MAX", "5566778899", 512);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Digital Innovation One");
